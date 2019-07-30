aba9e96702b2a6b5ee6178116e38def857a1e48e
Boris Babic <boris.ivan.babic@gmail.com>
2019-07-30 17:23:04 +0200

    Bump version

---
+++ release.nix
@@ -0,0 +1,12 @@
+#   # Enter the environment
+#   nix-shell release.nix
+#
+#   # create the package
+#   python setup.py bdist # system specific
+#   python setup.py sdist # for all 
+#
+#   # upload you package
+#   twine upload dist/project_name-x.y.z.tar.gz
+with import <nixpkgs> {};
+
+(pkgs.python36.withPackages (ps: with ps; [twine setuptools])).env
