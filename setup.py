249cb25072f4cee162265d209590a0ead49605d9
Sofia Paixão <anasofiapaixao@gmail.com>
2020-01-16 23:16:05 +0000

    no egg

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.9.1',
     packages=['browser_cookie3'],
     package_dir={'browser_cookie3' : '.'}, # look for package contents in current directory
+    zip_safe=False,
     author='Boris Babic',
     author_email='boris.ivan.babic@gmail.com',
     description='Loads cookies from your browser into a cookiejar object so can download with urllib and other libraries the same content you see in the web browser.',
