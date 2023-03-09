f20481545e9ad98b5af08eff9ca36a4ee0bcebf4
Ibrahim Rafi <rafiibrahim8@hotmail.com>
2023-03-09 16:32:56 +0600

    fixed #158

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='browser-cookie3',
-    version='0.17.0',
+    version='0.17.1',
     packages=['browser_cookie3'],
     # look for package contents in current directory
     package_dir={'browser_cookie3': '.'},
@@ -10,6 +10,11 @@ setup(
     author_email='boris.ivan.babic@gmail.com',
     description='Loads cookies from your browser into a cookiejar object so can download with urllib and other libraries the same content you see in the web browser.',
     url='https://github.com/borisbabic/browser_cookie3',
-    install_requires=['lz4', 'pycryptodomex', 'dbus-python'],
+    install_requires=[
+        'lz4',
+        'pycryptodomex',
+        'dbus-python; python_version < "3.7" and ("bsd" in sys_platform or sys_platform == "linux")',
+        'jeepney; python_version >= "3.7" and ("bsd" in sys_platform or sys_platform == "linux")'
+    ],
     license='lgpl'
 )
