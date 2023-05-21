c50a525324d5ce51330a64204cae384308d466f6
Ibrahim Rafi <rafiibrahim8@hotmail.com>
2023-05-21 15:24:42 +0600

    added test base10

--- tests/test_browsers.py
+++ tests/test_browsers.py
@@ -84,7 +84,6 @@ class Test(unittest.TestCase):
         if self.__headless:
             options.add_argument('--headless')
             options.add_argument('--disable-gpu')
-            options.add_argument('--no-sandbox')
         
         self.driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
     
